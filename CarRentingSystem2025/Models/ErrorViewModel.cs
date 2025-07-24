3e1b10ab3b49bd2970ea472d0a5c49c2d306751a
Teodor-Dimov <t.dimov7419@gmail.com>
2025-07-24 12:14:42 +0300

    Add project files.

---
+++ CarRentingSystem2025/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarRentingSystem2025.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
